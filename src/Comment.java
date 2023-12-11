81db189a77f4e3a1332170d534fb81f4a66119b8
sohee <sohee5997@naver.com>
2023-12-11 22:49:21 +0900

    study: 01 - hello java

---
+++ src/Comment.java
@@ -0,0 +1,11 @@
+public class Comment {
+    public static void main(String[] args) {
+        System.out.println("hello java 1"); // hello java 1을 출력합니다.
+        // System.out.println("hello java 2");
+
+        /* 여러 줄 주석
+        System.out.println("hello java 3");
+        System.out.println("hello java 4");
+         */
+    }
+}
---
+++ src/HelloJava.java
@@ -0,0 +1,10 @@
+public class HelloJava {
+    // 자바는 대소문자를 구분한다!
+    // public class HelloJava -> HelloJava 클래스를 의미
+    // 지금은 단순히 HelloJava.java 라는 파일을 만들었다고 이해
+    // 파일명과 클래스명은 같아야 한다
+    public static void main(String[] args) {    // main 메서드
+        // main은 프로그램의 시작점이다.
+        System.out.println("Hello Java");
+    }
+}
